9222ea3c44a592059e724e92d952529599cb740c
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.com>
2020-09-29 01:49:19 -0400

    Update controls

--- controls/V-32480.rb
+++ controls/V-32480.rb
@@ -46,7 +46,4 @@ control "V-32480" do
   tag "cci": ["CCI-000803"]
   tag "nist": ["IA-7", "Rev_4"]
 
-  describe command('openssl version') do
-    its('stdout') { should match 'fips' }
-  end
 end
